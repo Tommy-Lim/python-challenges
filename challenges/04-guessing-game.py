ee93068a40939f28d94c870953862ce74a83a8d9
Tommy-Lim <tlim24@gmail.com>
2017-02-07 10:49:49 -0800

    finished guessing game

--- challenges/04-guessing-game.py
+++ challenges/04-guessing-game.py
@@ -1,7 +1,22 @@
 from random import randint
 
-MIN = 1
-MAX = 5
+def guessing_game():
+    MIN = 1
+    MAX = 5
+    number = randint(MIN, MAX)
+    is_playing = True
 
-print("Guess a number between %d and %d" % (MIN, MAX))
-print("Delete me. Here's a random number:", randint(MIN, MAX))
+    while is_playing:
+        guess = input("What's your guess? ")
+        if int(guess) > number:
+            print("Try again, lower!")
+        elif int(guess) < number:
+            print("Try again, higher!")
+        elif int(guess) == number:
+            print("You got it!")
+            is_playing = False
+
+    print("Guess a number between {} and {}".format(MIN, MAX))
+    print("Delete me. Here's a random number: {}".format(number))
+
+guessing_game()
